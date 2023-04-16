71402cb8119f2bf3145beea725049f105e1ba7c3
erika-goodwin <shellvoerika@gmail.com>
2023-04-16 15:48:55 -0700

    Add | Mongoose setting

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", true);
 const connect = mongoose.connect(process.env.MONGODB_URL, {
   dbName: "portfolio",
 });
 
 connect
   .then(() => console.log("Connected to DB"))
-  .catch((err) => console.log('db connection err: ', err));
+  .catch((err) => console.log("db connection err: ", err));
